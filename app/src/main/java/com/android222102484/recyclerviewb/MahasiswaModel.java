2884b6f0697ef01bbeb4f995da06ebb5021d4624
Keyyy <rizkyabr698@gmail.com>
2025-06-04 14:04:06 +0700

    first commit

---
+++ app/src/main/java/com/android222102484/recyclerviewb/MahasiswaModel.java
@@ -0,0 +1,42 @@
+package com.android222102484.recyclerviewb;
+
+public class MahasiswaModel
+{
+    private String NIM, Nama, JenisKelamin, JP;
+
+    private MahasiswaModel() {
+
+    }
+
+    public String getNIM() {
+        return NIM;
+    }
+
+    public void setNIM(String NIM) {
+        this.NIM = NIM;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String nama) {
+        Nama = nama;
+    }
+
+    public String getJenisKelamin() {
+        return JenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        JenisKelamin = jenisKelamin;
+    }
+
+    public String getJP() {
+        return JP;
+    }
+
+    public void setJP(String JP) {
+        this.JP = JP;
+    }
+}
